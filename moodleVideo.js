8f0eea823ddbc6d060ede559105a2dea9f997920
翁梓薰 <77092651+EricWeng0527@users.noreply.github.com>
2023-10-19 02:17:52 +0800

    Add files via upload

--- moodleVideo.js
+++ moodleVideo.js
@@ -1,10 +1,12 @@
-let htmlContent = document.documentElement.outerHTML;
-let regex = /https:\/\/.*?\.mp4"/g;
-let matches = htmlContent.match(regex);
-if(matches){
-    url = matches[0].replace(/"/g, "");
-    window.open(url);
-}else{
-    alert("本網頁無影片!");
-}
+(function() {
+    let htmlContent = document.documentElement.outerHTML;
+    let regex = /https:\/\/.*?\.mp4"/g;
+    let matches = htmlContent.match(regex);
+    if(matches){
+        url = matches[0].replace(/"/g, "");
+        window.open(url);
+    }else{
+        alert("本網頁無影片!");
+    }
+})();
 //By Eric form NCKUEE 2023
