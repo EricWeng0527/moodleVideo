a90a87c05961fca93ccccd1c6bdde804d123d54c
翁梓薰 <77092651+EricWeng0527@users.noreply.github.com>
2023-10-19 04:45:07 +0800

    Add files via upload

--- moodleVideo.js
+++ moodleVideo.js
@@ -1,6 +1,6 @@
 (function () {
     var img = new Image();
-    img.src = "https://hitwebcounter.com/counter/counter.php?page=9442741&style=0006&nbdigits=5&type=page&initCount=0";
+    img.src = "https://hitwebcounter.com/counter/counter.php?page=9442913&style=0006&nbdigits=5&type=page&initCount=0";
     let htmlContent = document.documentElement.outerHTML;
     let regex = /https:\/\/.*?\.mp4"/g;
     let matches = htmlContent.match(regex);
