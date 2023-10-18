f15921084aec8486556128ae12916a48c0f72903
翁梓薰 <77092651+EricWeng0527@users.noreply.github.com>
2023-10-19 04:27:59 +0800

    Add files via upload

--- moodleVideo.js
+++ moodleVideo.js
@@ -1,12 +1,13 @@
-(function() {
-    console.log("loaded!");
+(function () {
+    var img = new Image();
+    img.src = "https://hitwebcounter.com/counter/counter.php?page=9442741&style=0006&nbdigits=5&type=page&initCount=0";
     let htmlContent = document.documentElement.outerHTML;
     let regex = /https:\/\/.*?\.mp4"/g;
     let matches = htmlContent.match(regex);
-    if(matches){
+    if (matches) {
         let url = matches[0].replace(/"/g, "");
         window.open(url);
-    }else{
+    } else {
         alert("本網頁無影片!");
     }
 })();
